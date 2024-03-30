446311bca1a5c992831ae97d8e0e6afafa1fe1c8
Raj Beladiya <rajbeladiya59@gmail.com>
2024-03-31 00:32:40 +0530

    Fix: The trans query was not working for the date

--- gatex-email-notification.py
+++ gatex-email-notification.py
@@ -45,15 +45,15 @@ def check_and_send_messages(cursor,connection):
     logger.info("Function check_and_send_messages called.")
     # Assuming local_time is the current time in IST
     local_time = datetime.now(tz.gettz('Asia/Dubai'))
-    
+
     # Calculating "two hours ago"
     two_hours_ago = local_time - timedelta(hours=2)
     two_hours_ago_time_str = two_hours_ago.strftime('%H:%M:%S')
-    
+
     # Formatting today's date
     today_date_str = local_time.strftime('%Y-%m-%d')
 
-    logger.info(f"Time two hours ago in IST (HH:MM:SS): {two_hours_ago_time_str}")
+    logger.info(f"Time two hours ago in IST (HH:MM:SS) and the date is: {two_hours_ago_time_str} and {today_date_str}")
     logger.info("Querying customer_master table.")
     customer_query = "SELECT customer_id, device_id, contact_person_emailID FROM customer_master"
     cursor.execute(customer_query)
@@ -73,29 +73,28 @@ def check_and_send_messages(cursor,connection):
             logger.error(f"JSON decoding error for customer {customer_id}")
         except ValueError:
             logger.error(f"Value error for customer {customer_id}, device ID: {device_id}")
-    
+
     logger.info("Querying trans table with date and email_sent filter.")
     trans_query = """
-    SELECT device_id,batch_id, entry_time, visitor_image_thumbnail
+    SELECT device_id,batch_id, entry_time
     FROM trans
     WHERE exit_time IS NULL 
     AND entry_time <= %s
-    AND date = %s
     AND email_sent = 0
     """
-    cursor.execute(trans_query, (two_hours_ago_time_str, today_date_str))
+    cursor.execute(trans_query, (two_hours_ago_time_str,))
     trans_data = cursor.fetchall()
     logger.info(f"Trans table data: {trans_data}")
-    
-    for (device_id,batch_id, entry_time, visitor_image_thumbnail) in trans_data:
+
+    for (device_id,batch_id, entry_time) in trans_data:
         logger.info(f"Processing trans table device ID: {device_id} and the customer_devices are {customer_devices}")
         # Make sure to convert device_id to integer before comparison
         if int(device_id) in customer_devices:
             contact_email = customer_devices[int(device_id)]
             logger.info(f"Device ID {device_id} found in customer_devices.")
-            
+
             device_id_json = json.dumps([str(device_id)])  # Ensure device_id is converted to string and then to JSON array format
-            
+            # device_id_json=json.dumps([str(111)]) 
             location_query = """
             SELECT lm.project_name, lm.building_name
             FROM location_master lm
@@ -103,12 +102,12 @@ def check_and_send_messages(cursor,connection):
             """
             cursor.execute(location_query, (device_id_json,))
             location_info = cursor.fetchone()
-            
+
             logger.info(f"Location info: {location_info}")
-            
+
             project_name, building_name = location_info if location_info else ("Unknown", "Unknown")
             logger.info(f"Location info for device ID {device_id}: Project Name - {project_name}, Building Name - {building_name}")
-    
+
             # Assuming contact_email is determined before this code block
             subject = "Gatex Visitor Alert !"
             body = f"""\
@@ -123,17 +122,17 @@ def check_and_send_messages(cursor,connection):
 
             project_name, building_name = location_info if location_info else ("Unknown", "Unknown")
             send_email(contact_email, subject, body)
-            
+
             # Update email_sent to 1 for the current record
             update_query = """
             UPDATE trans
             SET email_sent = 1
-            WHERE device_id = %s AND date = %s AND entry_time = %s
+            WHERE device_id = %s AND entry_time = %s
             """
-            cursor.execute(update_query, (device_id, today_date_str, entry_time))
+            cursor.execute(update_query, (device_id, entry_time))
             logger.info(f"Updated email_sent for device ID: {device_id}, entry time: {entry_time}")
             connection.commit()
-   
+
 
 
 def lambda_handler(event, context):
@@ -143,7 +142,7 @@ def lambda_handler(event, context):
         logger.info("Connecting to the database.")
         connection = mysql.connector.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
         logger.info("Database connection established.")
-        
+
         with connection.cursor() as cursor:
             logger.info("Cursor created.")
             check_and_send_messages(cursor,connection)  # Pass the IST datetime object to the function
